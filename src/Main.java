187a296a397df1583a756eb943e8923166a2c7e0
Ivan Gutorov <gutorov.ia1@gmail.com>
2025-03-12 03:09:56 +0400

    add mpi shit

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,54 @@
 public class Main {
     public static void main(String[] args) {
         try {
-            if (args.length != 3)
-                throw new IllegalArgumentException("Put fucking numbers into a fucking command line.");
+//            temporary убрал ибо хз чо там в mpi
+//            if (args.length != 4) // размеры матрицы, количество цифр в числе (хз зачем сделал, но уже лень убирать) и аргумент сколько лаб запускать
+//                throw new IllegalArgumentException("Put fucking numbers into a fucking command line.");
 
-            long startTime, endTime;
-            int min;
             Matrix matrix = new Matrix(Integer.parseInt(args[0]),
                     Integer.parseInt(args[1]), Integer.parseInt(args[2]));
             System.out.println(matrix);
 
-            startTime = System.nanoTime();
-            min = matrix.processMatrixSingleThread();
-            endTime = System.nanoTime();
-            printResults("SingleThread", min, endTime - startTime);
-
-            startTime = System.nanoTime();
-            min = matrix.processMatrixThreadPoolExecutor();
-            endTime = System.nanoTime();
-            printResults("ThreadPoolExecutor", min, endTime - startTime);
-
-            startTime = System.nanoTime();
-            min = matrix.processMatrixForkJoinPoll();
-            endTime = System.nanoTime();
-            printResults("ForkJoinPoll", min, endTime - startTime);
+            if (args[3].contains("1"))
+                demoLab1(matrix);
+            if (args[3].contains("2"))
+                demoLab2(matrix, args);
 
         } catch (NumberFormatException e) {
             System.out.println("Put numbers, not a letters, stupid idiot.");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    private static void demoLab1(Matrix matrix) {
+        long startTime, endTime;
+        int min;
+
+        startTime = System.nanoTime();
+        min = matrix.processMatrixSingleThread();
+        endTime = System.nanoTime();
+        printResults("SingleThread", min, endTime - startTime);
+
+        startTime = System.nanoTime();
+        min = matrix.processMatrixThreadPoolExecutor();
+        endTime = System.nanoTime();
+        printResults("ThreadPoolExecutor", min, endTime - startTime);
+
+        startTime = System.nanoTime();
+        min = matrix.processMatrixForkJoinPoll();
+        endTime = System.nanoTime();
+        printResults("ForkJoinPoll", min, endTime - startTime);
+    }
+
+    private static void demoLab2(Matrix matrix, String[] args) {
+        long startTime, endTime;
+        int min;
+
+        startTime = System.nanoTime();
+        min = matrix.processMatrixMPI(args);
+        endTime = System.nanoTime();
+        printResults("MPI", min, endTime - startTime);
     }
 
     private static void printResults(String algorithm, int min, long time) {
--- src/Matrix.java
+++ src/Matrix.java
@@ -1,3 +1,5 @@
+import mpi.MPI;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -54,9 +56,117 @@ public class Matrix {
         }
     }
 
+    public int processMatrixForkJoinPoll() {
+        return new ForkJoinMinFinder().findMin();
+    }
+
+    public int processMatrixMPI(String args[]) {
+        MPI.Init(args);
+        int rank = MPI.COMM_WORLD.Rank();
+        int size = MPI.COMM_WORLD.Size();
+        int[] globalMin = new int[1];
+
+        if (rank == 0) {
+            int[] dims = new int[]{matrix.length, matrix[0].length};
+            for (int i = 1; i < size; i++)
+                MPI.COMM_WORLD.Send(dims, 0, 2, MPI.INT, i, 0);
+
+
+            int rowsPerProcess = matrix.length / size;
+            int remainder = matrix.length % size;
+
+            int startRow = rowsPerProcess + (rank < remainder ? 1 : 0);
+            int endRow = Math.min(matrix.length, startRow + rowsPerProcess + (rank + 1 < remainder ? 1 : 0));
+
+            for (int i = 1; i < size; i++) {
+                int processStartRow = i * rowsPerProcess + Math.min(i, remainder);
+                int processEndRow = Math.min(matrix.length, processStartRow + rowsPerProcess + (i < remainder ? 1 : 0));
+                int numRows = processEndRow - processStartRow;
+
+                if (numRows > 0)
+                    for (int r = processStartRow; r < processEndRow; r++)
+                        MPI.COMM_WORLD.Send(matrix[r], 0, matrix[r].length, MPI.INT, i, r);
+            }
+
+            int localMin = findLocalMin(0, startRow);
+
+            int[] remoteMin = new int[1];
+            for (int i = 1; i < size; i++) {
+                MPI.COMM_WORLD.Recv(remoteMin, 0, 1, MPI.INT, i, 99);
+                localMin = Math.min(localMin, remoteMin[0]);
+            }
+
+            globalMin[0] = localMin;
+        } else {
+            int[] dims = new int[2];
+            MPI.COMM_WORLD.Recv(dims, 0, 2, MPI.INT, 0, 0);
+            int rows = dims[0];
+            int cols = dims[1];
+
+            int rowsPerProcess = rows / size;
+            int remainder = rows % size;
+            int startRow = rank * rowsPerProcess + Math.min(rank, remainder);
+            int endRow = Math.min(rows, startRow + rowsPerProcess + (rank < remainder ? 1 : 0));
+            int numRows = endRow - startRow;
+
+            int[][] localMatrix = new int[numRows][cols];
+
+            if (numRows > 0) {
+                for (int r = 0; r < numRows; r++) {
+                    MPI.COMM_WORLD.Recv(localMatrix[r], 0, cols, MPI.INT, 0, startRow + r);
+                }
+
+                int localMin = Integer.MAX_VALUE;
+                for (int i = 0; i < numRows; i++) {
+                    int globalRow = startRow + i;
+                    for (int j = globalRow + 1; j < cols; j++) {
+                        localMin = Math.min(localMin, localMatrix[i][j]);
+                    }
+                }
+
+                int[] minArray = new int[]{localMin};
+                MPI.COMM_WORLD.Send(minArray, 0, 1, MPI.INT, 0, 99);
+            }
+        }
+
+        MPI.Finalize();
+        if (rank == 0)
+            return globalMin[0];
+        else
+            return 0;
+    }
+
+    private int findLocalMin(int startRow, int endRow) {
+        if (startRow >= matrix.length - 1) {
+            return Integer.MAX_VALUE;
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (int i = startRow; i < endRow && i < matrix.length - 1; i++) {
+            for (int j = i + 1; j < matrix[i].length; j++) {
+                min = Math.min(min, matrix[i][j]);
+            }
+        }
+        return min;
+    }
+
+    @Override
+    public String toString() {
+        if (matrix.length > 100)
+            return "Matrix Size: " + matrix.length + "x" + matrix[0].length;
+
+        StringBuilder sb = new StringBuilder();
+        for (int[] ints : matrix) {
+            for (int anInt : ints)
+                sb.append(String.format("%" + numberOfChars + "d ", anInt));
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
     private class ForkJoinMinFinder extends RecursiveTask<Integer> {
-        private final int start, end;
         private static final int THRESHOLD = 10;
+        private final int start, end;
 
         public ForkJoinMinFinder() {
             this.start = 0;
@@ -92,23 +202,5 @@ public class Matrix {
                 return pool.invoke(new ForkJoinMinFinder());
             }
         }
-    }
-
-    public int processMatrixForkJoinPoll() {
-        return new ForkJoinMinFinder().findMin();
-    }
-
-    @Override
-    public String toString() {
-        if (matrix.length > 100)
-            return "Matrix Size: " + matrix.length + "x" + matrix[0].length;
-
-        StringBuilder sb = new StringBuilder();
-        for (int[] ints : matrix) {
-            for (int anInt : ints)
-                sb.append(String.format("%" + numberOfChars + "d ", anInt));
-            sb.append("\n");
-        }
-        return sb.toString();
     }
 }
